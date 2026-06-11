6cde93ec26bf46e085f7b3f1e1e6c15e28f19ea5
niniutopia <nicchan707@gmail.com>
2026-06-11 17:33:23 +0200

    aggiornato confronta giocatori

--- main.py
+++ main.py
@@ -78,6 +78,7 @@ st.markdown("""
 
 # Sidebar con info
 with st.sidebar:
-    st.title("Steam Buddies")
+    st.title("Steam Buddy")
+    st.markdown("Analizza le tue statistiche di gioco e confrontati con i tuoi amici!")
     st.markdown("---")
     st.caption("Alimentato da Steam Web API")
